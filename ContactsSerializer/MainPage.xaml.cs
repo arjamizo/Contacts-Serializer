60fdeb83cd961af265791aa3eaa1cd2514fed1f8
Artur <arjamizo@gmail.com>
2012-04-12 18:45:29 +0200

    v0.1

--- ContactsSerializer/MainPage.xaml.cs
+++ ContactsSerializer/MainPage.xaml.cs
@@ -11,9 +11,11 @@ using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using Microsoft.Phone.Controls;
 using System.Threading;
+using Microsoft.Phone.UserData;
 
 namespace ContactsSerializer
 {
+
     public partial class MainPage : PhoneApplicationPage
     {
         // Constructor
@@ -25,11 +27,38 @@ namespace ContactsSerializer
 
         private void getContacts_Click(object sender, RoutedEventArgs e)
         {
-            (sender as Button).Content = "cze";
+            (sender as Button).Content = "pobrano";
             (sender as Button).UpdateLayout();
-            (FindName("result") as TextBox).Text += "How are you?\n";
+            (sender as Button).Click -= getContacts_Click; //remove click handler
+            Thread.Sleep(1000);
+            (sender as Button).Visibility = Visibility.Collapsed;
+            fillContactsTextBox();
+            //MessageBox.Show("udało się");
+        }
+        String str="";
+        private string fillContactsTextBox()
+        {
+            Contacts cons = new Contacts();
+            str = "";
+            cons.SearchCompleted += new EventHandler<ContactsSearchEventArgs>(Contacts_SearchCompleted);
+            cons.SearchAsync(String.Empty, FilterKind.None, "Contacts Test #1");
+            return str;
+        }
+
+        void Contacts_SearchCompleted(object sender, ContactsSearchEventArgs e)
+        {
+            //Do something with the results.
+            //MessageBox.Show(e.Results.Count().ToString());
+            System.Text.StringBuilder sb = new System.Text.StringBuilder();
+            foreach (Contact con in e.Results)
+            {
+                sb.AppendLine("\"" + con.DisplayName + "\", "+con.PhoneNumbers.FirstOrDefault());
+            }
+
+            str = sb.ToString(); 
+            (FindName("result") as TextBox).Text = str;
             (FindName("result") as TextBox).UpdateLayout();
-            MessageBox.Show("udało się");
+            //MessageBox.Show(str);
         }
     }
 }
